964aca73d1a27ab160cf21ff192b0521b1c1f2c9
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-12-06 10:52:30 -0800

    tests passing

---
+++ lib/nursery_rhyme.rb
@@ -0,0 +1,31 @@
+class NurseryRhyme
+
+  def recite
+    1.upto(12).map {|i| line(i)}.join "\n"
+  end
+
+  def line number
+    "This is the #{pieces.take(number).reverse.join(' ')}\n"
+  end
+
+  private
+
+  def pieces
+    [
+      "house that Jack built.",
+      "malt that lay in the",
+      "rat that ate the",
+      "cat that killed the",
+      "dog that worried the",
+      "cow with the crumpled horn that tossed the",
+      "maiden all forlorn that milked the",
+      "man all tattered and torn that kissed the",
+      "priest all shaven and shorn that married the",
+      "cock that crowed in the morn that woke the",
+      "farmer sowing his corn that kept the",
+      "horse and the hound and the horn that belonged to the"
+    ]
+  end
+end
+
+
