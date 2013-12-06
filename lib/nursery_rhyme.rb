4731942a1b72b4aefb8f0b4a1b1c2272831fd72c
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-12-06 13:29:31 -0800

    ready for randomize

--- lib/nursery_rhyme.rb
+++ lib/nursery_rhyme.rb
@@ -1,35 +1,64 @@
 class NurseryRhyme
+  attr_reader :randomize
+
+  def initialize randomize=false
+    @randomize = randomize
+  end
 
   def recite
-    1.upto(12).map {|i| line(i)}.join "\n"
+    if randomize
+
+    else
+      1.upto(12).map {|i| line i }.join "\n"
+    end
   end
 
   def line number
     "This is the #{sections_for_line number}\n"
   end
 
-  private
-
   def sections_for_line number
     sections.take(number).reverse.join ' '
   end
 
   def sections
+    12.times.map do |number|
+      "#{subjects[number]} that #{actions[number]}"
+    end
+  end
+
+  def subjects
     [
-      "house that Jack built.",
-      "malt that lay in the",
-      "rat that ate the",
-      "cat that killed the",
-      "dog that worried the",
-      "cow with the crumpled horn that tossed the",
-      "maiden all forlorn that milked the",
-      "man all tattered and torn that kissed the",
-      "priest all shaven and shorn that married the",
-      "cock that crowed in the morn that woke the",
-      "farmer sowing his corn that kept the",
-      "horse and the hound and the horn that belonged to the"
+      "house",
+      "malt",
+      "rat",
+      "cat",
+      "dog",
+      "cow with the crumpled horn",
+      "maiden all forlorn",
+      "man all tattered and torn",
+      "priest all shaven and shorn",
+      "cock that crowed in the morn",
+      "farmer sowing his corn",
+      "horse and the hound and the horn"
     ]
   end
+
+  def actions
+    [
+      "Jack built.",
+      "lay in the",
+      "ate the",
+      "killed the",
+      "worried the",
+      "tossed the",
+      "milked the",
+      "kissed the",
+      "married the",
+      "woke the",
+      "kept the",
+      "belonged to the"
+    ]
+  end
+
 end
-
-
